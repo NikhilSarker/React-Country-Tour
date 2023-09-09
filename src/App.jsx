401e49f0adf13b9779515a4c431de09f8c23e03f
Nikhil Sarker <100969232+NikhilSarker@users.noreply.github.com>
2023-09-09 11:29:44 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold'>React Country Tour</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
